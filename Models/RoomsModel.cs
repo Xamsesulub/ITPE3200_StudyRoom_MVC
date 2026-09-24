93d6820701de14786d3c6259adc91750e6e5da09
Erik Bekkeli <bekkeli.erik@outlook.com>
2026-09-24 16:01:45 +0200

    dotnet EF kommandoer kjørt slik at vi har en db fil

--- Models/RoomsModel.cs
+++ Models/RoomsModel.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MVC.Models;
 
 public class RoomsModel
 {
+    //Data Annotation so EF knows this is a primary key
+    [Key]
     public int RoomId { get; set; }
 
     public string RoomBuilding { get; set; }
