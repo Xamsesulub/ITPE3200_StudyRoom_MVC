fb44d99f08803ef5b66c636eeb75760e8e90fa2d
ebekkeli <bekkeli.erik@outlook.com>
2026-09-24 13:37:50 +0200

    gitignore

--- obj/Debug/net10.0/MVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10472c596c373e01ec3c7e1088e7b4674abb81a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f6235967802af4a07717afc3efea616fae0395")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
